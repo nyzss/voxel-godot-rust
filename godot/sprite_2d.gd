f34027d752a09304e8e15e859fe5392aff19b2e8
nyzss <oknakoca@gmail.com>
2025-12-27 13:00:18 +0100

    init

---
+++ godot/sprite_2d.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#pass # Replace with function body.
+	print("hello world from gdscript")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
