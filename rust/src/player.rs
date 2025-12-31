8112110974aba42b47e5ff79a977bd6012fdd62d
nyzss <oknakoca@gmail.com>
2025-12-31 16:44:11 +0100

    feat: add flying

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -15,6 +15,7 @@ struct Player {
     speed: f32,
     #[export]
     jump_velocity: f32,
+    flying: bool,
 
     eye_camera: OnReady<Gd<Camera3D>>,
     head: OnReady<Gd<Node3D>>,
@@ -30,6 +31,7 @@ impl ICharacterBody3D for Player {
             base,
             speed: 5.0,
             jump_velocity: 4.5,
+            flying: false,
 
             head: OnReady::from_node("Head"),
             eye_camera: OnReady::from_node("Head/EyeCamera"),
@@ -41,22 +43,36 @@ impl ICharacterBody3D for Player {
     fn physics_process(&mut self, delta: f64) {
         let input = Input::singleton();
 
+        if input.is_action_just_pressed("flying") {
+            self.flying = !self.flying;
+        }
+
         let mut velocity = self.base().get_velocity();
         if !self.base().is_on_floor() {
-            velocity += self.base().get_gravity() * delta as f32;
+            if self.flying {
+                velocity = Vector3::ZERO;
+            } else {
+                velocity += self.base().get_gravity() * delta as f32;
+            }
         }
-        if input.is_action_just_pressed("jump") && self.base().is_on_floor() {
+        if input.is_action_pressed("jump") && self.base().is_on_floor() {
             velocity.y = self.jump_velocity;
         }
 
         let input_dir = input.get_vector("move_left", "move_right", "move_up", "move_down");
-        let mut direction =
-            self.base().get_transform().basis * Vector3::new(input_dir.x, 0., input_dir.y);
+
+        let mut direction = self.eye_camera.get_global_transform().basis
+            * Vector3::new(input_dir.x, 0., input_dir.y);
+
         if direction.length() > 0.0 {
             direction = direction.normalized();
 
-            velocity.x = direction.x * self.speed;
-            velocity.z = direction.z * self.speed;
+            if self.flying {
+                velocity = direction * self.speed;
+            } else {
+                velocity.x = direction.x * self.speed;
+                velocity.z = direction.z * self.speed;
+            }
         } else {
             velocity.x = move_toward(velocity.x as f64, 0. as f64, self.speed as f64) as f32;
             velocity.z = move_toward(velocity.z as f64, 0. as f64, self.speed as f64) as f32;
